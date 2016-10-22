1790d60b97dc53452211c3c73bb84936202e7c67
sbrajagopal26 <sbrajagopal26@users.noreply.github.com>
2016-10-22 16:03:45 +0200

    Add files via upload

---
+++ JenkinsDemo/src/demojenkins/DemoJenkinsJobs.java
@@ -0,0 +1,10 @@
+package demojenkins;
+
+import org.testng.annotations.Test;
+
+public class DemoJenkinsJobs {
+	@Test
+	public void testJenkins(){
+		System.out.println("Welcome to jenkins world");
+	}
+}
